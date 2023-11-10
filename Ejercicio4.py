2c6341bfa5b2147a7b0d1304429757d2508f8c24
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-10 15:55:59 +0100

    Create Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,40 @@
+x = {}
+
+
+y = input("ingrese el nombre")
+x["Nombre"] = y
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
+
+
+b = input("ingrese su edad")
+x["Edad"] = b
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
+       
+d = input("ingrese su sexo")
+x["sexo"] = d
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
+
+
+e = input("ingrese su telefono")
+x["telefono"] = e
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
+
+
+f = input("ingrese su correo electronico")
+x["correo"] = f
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
